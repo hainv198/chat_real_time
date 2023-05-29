586a9c877b40b8b7eadac4863dace202c031e986
MSI <hainvth161197@gmail.com>
2023-05-29 09:25:11 +0700

    hainv add button

--- messenger_real_time/app/(site)/components/AuthForm.tsx
+++ messenger_real_time/app/(site)/components/AuthForm.tsx
@@ -3,6 +3,8 @@ import {useCallback, useState} from "react";
 import {FieldValues, SubmitHandler, useForm} from "react-hook-form";
 import Input from "@/app/components/inputs/Input";
 import Button from "@/app/components/Button";
+import AuthSocialButton from "@/app/(site)/components/AuthSocialButton";
+import {BsGithub, BsGoogle} from "react-icons/all";
 
 // Định nghĩa kiểu đầu vào cho form
 type Variant = 'LOGIN' | 'REGISTER';
@@ -76,11 +78,33 @@ const AuthForm = () => {
                            errors={errors}
                     />
                     <div>
-                        <Button danger>
-                            Submit
+                        <Button disabled={isLoading} type="submit" fullWidth>
+                            {variant === 'LOGIN' ? 'Sign' : 'Register'}
                         </Button>
                     </div>
                 </form>
+                <div className="mt-6">
+                    <div className="relative">
+                        <div className="absolute inset-0 flex items-center">
+                            <div className="w-full border-t border-gray-300" />
+                        </div>
+                    </div>
+                    <div className="relative flex justify-center text-sm">
+                      <span className="bg-white px-2 text-gray-500">
+                        Or continue with
+                      </span>
+                    </div>
+                </div>
+                <div className="mt-6 flex gap-2">
+                    <AuthSocialButton
+                        icon={BsGithub}
+                        onClick={() => socialAction('github')}
+                    />
+                    <AuthSocialButton
+                        icon={BsGoogle}
+                        onClick={() => socialAction('google')}
+                    />
+                </div>
             </div>
       </div>
     );
